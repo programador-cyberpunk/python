f7fd5c6f852b6008c80008101cdc82748c0d10ec
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2025-03-17 17:57:56 -0300

    Add files via upload

---
+++ tipos.py
@@ -0,0 +1,21 @@
+
+#tipos de dados
+a = 30
+b = 25.4
+dobro = a * 2
+c = b - a
+print(f'O dobro de {a} eh {dobro} e seu tipo eh {type(dobro)} ')
+print(f'A diferença entre {b} e {a} eh {c} ,e o seu tipo eh {type(c)}')
+
+#tipo int
+n = 23.58
+print(int(n))
+
+#string
+p = 'palavra'
+print(p)
+print(p[0],p[1],p[2],p[3],p[4],p[5],p[6], sep='-')
+
+#lista
+bagulhos = ['x', 1, 'teste', 3.14, True]
+print(bagulhos[4])
